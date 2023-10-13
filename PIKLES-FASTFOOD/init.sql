fdfa74587d641b6386e94530fa80640a35a394a8
Luís Felipe Amengual Tatsch <lfelipe_at@yahoo.com.br>
2023-10-13 14:52:03 -0300

    Ajuste na criação das tabelas

--- PIKLES-FASTFOOD/init.sql
+++ PIKLES-FASTFOOD/init.sql
@@ -1,25 +1,30 @@
 CREATE TABLE IF NOT EXISTS Cliente (
-  id INT NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(50) NOT NULL,
-  sobrenome VARCHAR(50) NOT NULL,
-  cpf VARCHAR(14) NOT NULL,
-  email VARCHAR(254) NOT NULL,
-  PRIMARY KEY (id)
+  Id INT NOT NULL AUTO_INCREMENT,
+  Nome VARCHAR(50) NOT NULL,
+  Sobrenome VARCHAR(50) NOT NULL,
+  Cpf VARCHAR(14) NOT NULL,
+  Email VARCHAR(254) NOT NULL,
+  PRIMARY KEY (Id)
 );
 
 CREATE TABLE IF NOT EXISTS Categoria (
-  id INT NOT NULL AUTO_INCREMENT,
-  nomeCategoria VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
+  Id INT NOT NULL AUTO_INCREMENT,
+  NomeCategoria VARCHAR(255) NOT NULL,
+  PRIMARY KEY (Id)
 );
 
 CREATE TABLE IF NOT EXISTS Produto (
-  id INT NOT NULL AUTO_INCREMENT,
-  codigoProduto INT NOT NULL,
-  nomeProduto VARCHAR(100) NOT NULL,
-  valorProduto FLOAT NOT NULL,
-  idCategoriaProduto INT NOT NULL,
-  descricaoProduto VARCHAR(500) NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT fk_categoria_produto FOREIGN KEY (idCategoriaProduto) REFERENCES Categoria(id)
+  Id INT NOT NULL AUTO_INCREMENT,
+  CodigoProduto INT NOT NULL,
+  NomeProduto VARCHAR(100) NOT NULL,
+  ValorProduto FLOAT NOT NULL,
+  Descricao VARCHAR(500) NOT NULL,
+  IdCategoria INT NOT NULL,
+  PRIMARY KEY (Id),
+  CONSTRAINT fk_categoria_produto FOREIGN KEY (IdCategoria) REFERENCES Categoria(Id)
 );
+
+INSERT INTO Categoria (NomeCategoria) VALUES ('Lanche');
+INSERT INTO Categoria (NomeCategoria) VALUES ('Acompanhamento');
+INSERT INTO Categoria (NomeCategoria) VALUES ('Bebida');
+INSERT INTO Categoria (NomeCategoria) VALUES ('Sobremesa');
